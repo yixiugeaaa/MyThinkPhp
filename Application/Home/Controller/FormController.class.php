8abfc36ed0d52e33b2a201008ef00f2b0f195cb4
yxg <yanxiguoa@126.com>
2015-09-11 16:15:40 +0800

    create数据

---
+++ Application/Home/Controller/FormController.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class FormController extends Controller{
+	
+	public function insert(){
+		$Form   =   D('Form');
+		/* if($Form->create()) {
+			$result =   $Form->add();
+			if($result) {
+				$this->success('数据添加成功！');
+			}else{
+				$this->error('数据添加错误！');
+			}
+		}else{
+			$this->error($Form->getError());
+		} */
+		$Form   =   D('Form');
+		$data['title']  =   'ThinkPHP';
+		$data['content']    =   '表单内容';
+		$Form->add($data);
+	}
+	
+}
