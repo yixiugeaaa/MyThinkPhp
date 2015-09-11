cb8cda56e6f2824006d65aa9ff43d169347c00dc
yxg <yanxiguoa@126.com>
2015-09-11 16:37:46 +0800

    更改README.md

---
+++ Application/Runtime/Cache/Home/d86ffc4c1353d30d0cfbfd2c48e280b4.php
@@ -0,0 +1,18 @@
+<?php if (!defined('THINK_PATH')) exit();?><table>
+ <tr>
+    <td>id:</td>
+    <td><?php echo ($data["id"]); ?></td>
+ </tr>
+ <tr>
+    <td>标题：</td>
+    <td><?php echo ($data["title"]); ?></td>
+ </tr>
+ <tr>
+    <td>内容：</td>
+    <td><?php echo ($data["content"]); ?></td>
+ </tr>
+  <tr>
+    <td>创建时间：</td>
+    <td><?php echo ($data["create_time"]); ?></td>
+ </tr>
+ </table>
